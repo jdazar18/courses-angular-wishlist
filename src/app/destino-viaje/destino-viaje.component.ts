3452ed9b48af85fd728956794718d19840c1a323
Julian Daza <jdazar@unal.edu.co>
2025-02-08 11:12:40 -0500

    feat: added index in each travel destination

--- src/app/destino-viaje/destino-viaje.component.ts
+++ src/app/destino-viaje/destino-viaje.component.ts
@@ -1,14 +1,16 @@
 import { Component, Input, HostBinding, EventEmitter, Output } from '@angular/core';
 import { DestinoViaje } from '../models/destino-viaje.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-destino-viaje',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './destino-viaje.component.html',
   styleUrl: './destino-viaje.component.css'
 })
 export class DestinoViajeComponent {
   @Input() destino? : DestinoViaje;
+  @Input('idx') position? : number;
   @HostBinding('attr.class') cssClass = 'col-md-4';
   @Output() clicked : EventEmitter<DestinoViaje>;
 
--- src/app/models/destino-viaje.model.ts
+++ src/app/models/destino-viaje.model.ts
@@ -1,9 +1,10 @@
 export class DestinoViaje {
 
     private selected: boolean = false;
+    public servicios: string[];
 
     constructor(public nombre: string, public url: string) {
-
+        this.servicios = ['Piscina', 'Desayuno'];
     }
 
     isSelected(): boolean {
