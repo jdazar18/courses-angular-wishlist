14faa6b237ec6e14a2a8e1f8917276facb17778b
Julian Daza <jdazar@unal.edu.co>
2025-02-08 11:41:59 -0500

    feat: created destino-detalle component

---
+++ src/app/destino-detalle/destino-detalle.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-destino-detalle',
+  imports: [],
+  templateUrl: './destino-detalle.component.html',
+  styleUrl: './destino-detalle.component.css'
+})
+export class DestinoDetalleComponent {
+
+}
