c6db72b2d169f892c6db9caed42bb9f0ac43fc9d
Julian Daza <jdazar@unal.edu.co>
2025-02-08 10:19:42 -0500

    feat: added destino-viaje and lista-destinos components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListaDestinosComponent } from "./lista-destinos/lista-destinos.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListaDestinosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
